d301fd25abf43bfb758b5801635578322ef34e2d
vinicius <vinybks@hotmail.com>
2020-03-27 16:19:00 -0300

    Implements API BackEnd

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+cont cors = require('cors')
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const port = 3001;
+app.listen(port);
+
+console.log(`Server is Running on port :: ${port}`)
