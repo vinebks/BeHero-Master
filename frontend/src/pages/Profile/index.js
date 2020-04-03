3ad03925e8789ff5eb1e5d26731c17a1a9975766
vinicius <vinybks@hotmail.com>
2020-04-03 12:24:09 -0300

    Implementing Profile Page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 
 import './styles.css';
@@ -18,6 +18,70 @@ function Profile() {
                     <FiPower size={18} color="#E02041"></FiPower>
                 </button>
             </header>
+
+            <h1>Casos Cadastrados</h1>
+            
+                <ul> 
+                    <li>
+                        <strong>CASO</strong>
+                        <p>Caso teste</p>
+
+                        <strong>DESCRIÇÃO</strong>
+                        <p>Descrição Teste</p>
+
+                        <strong>VALOR</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20}></FiTrash2>
+                        </button>
+                    </li>
+
+                    <li>
+                        <strong>CASO</strong>
+                        <p>Caso teste</p>
+
+                        <strong>DESCRIÇÃO</strong>
+                        <p>Descrição Teste</p>
+
+                        <strong>VALOR:</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20}></FiTrash2>
+                        </button>
+                    </li>
+
+                    <li>
+                        <strong>CASO</strong>
+                        <p>Caso teste</p>
+
+                        <strong>DESCRIÇÃO</strong>
+                        <p>Descrição Teste</p>
+
+                        <strong>VALOR</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20}></FiTrash2>
+                        </button>
+                    </li>
+
+                    <li>
+                        <strong>CASO</strong>
+                        <p>Caso teste</p>
+
+                        <strong>DESCRIÇÃO</strong>
+                        <p>Descrição Teste</p>
+
+                        <strong>VALOR</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20}></FiTrash2>
+                        </button>
+                    </li>
+                </ul>            
         </div>
     );
 }
